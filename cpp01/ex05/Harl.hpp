324a8a264005ae50212d987bf8960f967aacafcb
fabileo <76965192+leonfabi@users.noreply.github.com>
2024-01-03 11:33:04 +0100

    ex05

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkrug <fkrug@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/03 10:20:02 by fkrug             #+#    #+#             */
+/*   Updated: 2024/01/03 10:23:38 by fkrug            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+#include <string>
+
+class Harl{
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+	public:
+		Harl();
+		~Harl();
+		void complain(std::string level);
+};
+
+#endif
