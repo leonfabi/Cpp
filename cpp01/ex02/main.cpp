7262a575a3001fa4016a6f7e658ca42456215760
42fabileo <76965192+leonfabi@users.noreply.github.com>
2024-01-05 09:32:07 +0100

    ex02 update

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fkrug <fkrug@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 12:50:54 by fkrug             #+#    #+#             */
-/*   Updated: 2024/01/02 13:38:43 by fkrug            ###   ########.fr       */
+/*   Updated: 2024/01/05 09:31:03 by fkrug            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 int	main(void)
 {
-	const std::string	var = "HI THIS IS BRIAN";
-	const std::string	*stringPTR = &var;
-	const std::string	&stringREF = var;
+	const std::string	var = "HI THIS IS BRAIN";
+	const std::string*	stringPTR = &var;
+	const std::string&	stringREF = var;
 
 	std::cout << "The memory address of the string variable: " << &var << std::endl;
 	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
