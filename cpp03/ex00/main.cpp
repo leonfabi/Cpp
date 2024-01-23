e9a2f677ea3605db91012389f921564cf19b8c5a
42fabileo <76965192+leonfabi@users.noreply.github.com>
2024-01-23 15:49:07 +0100

    added test cases

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fkrug <fkrug@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 11:45:09 by fkrug             #+#    #+#             */
-/*   Updated: 2024/01/22 12:00:44 by fkrug            ###   ########.fr       */
+/*   Updated: 2024/01/23 15:47:29 by fkrug            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,20 @@ int	main(void)
 	{
 	ClapTrap	one("Fabian");
 
-
 	one.print();
-	one.attack("two");
-	one.takeDamage(20);
+	one.attack("Attack 1");
+	one.attack("Attack 2");
+	one.attack("Attack 3");
+	one.attack("Attack 4");
+	one.attack("Attack 5");
+	one.attack("Attack 6");
+	one.attack("Attack 7");
+	one.attack("Attack 8");
+	one.attack("Attack 9");
 	one.beRepaired(5);
 	one.print();
-
+	one.takeDamage(20);
+	one.print();
+	one.attack("Attack 10");
 	}
 }
