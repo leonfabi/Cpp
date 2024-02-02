fa213cc029bef410a7900b8e3c2596b6be213e33
42fabileo <76965192+leonfabi@users.noreply.github.com>
2024-02-02 14:55:15 +0100

    cpp03/ex03 update main

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fkrug <fkrug@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 18:00:00 by fkrug             #+#    #+#             */
-/*   Updated: 2024/02/01 21:54:27 by fkrug            ###   ########.fr       */
+/*   Updated: 2024/02/02 14:54:36 by fkrug            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,31 @@ int main(void){
     IMateriaSource* src = new MateriaSource();
     src->learnMateria(new Ice());
     src->learnMateria(new Cure());
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
     ICharacter* me = new Character("me");
     AMateria* tmp;
     tmp = src->createMateria("ice");
     me->equip(tmp);
     tmp = src->createMateria("cure");
     me->equip(tmp);
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
     ICharacter* bob = new Character("bob");
+    std::cout << "################################################\n";
     me->use(0, *bob);
     me->use(1, *bob);
+    me->use(2, *bob);
+    me->use(3, *bob);
+    me->unequip(1);
+    me->use(1, *bob);
+    me->equip(tmp);
+    me->use(1, *bob);
+    me->use(4, *bob);
+    std::cout << "################################################\n";
     delete bob;
     delete me;
     delete src;
     return 0;
-
 }
