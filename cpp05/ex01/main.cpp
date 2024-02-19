caf43b6959aef0c9435b1515a6bf8c599894f1e4
42fabileo <76965192+leonfabi@users.noreply.github.com>
2024-02-19 14:12:18 +0100

    cpp05/ex01 updated tests

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fkrug <fkrug@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 17:36:40 by fkrug             #+#    #+#             */
-/*   Updated: 2024/02/17 11:55:41 by fkrug            ###   ########.fr       */
+/*   Updated: 2024/02/19 13:54:11 by fkrug            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,18 @@
 
 int main(void){
     {
+    std::cout << "Test1:\nGenerate Form and sign it\n";
     Bureaucrat a("Max", 10);
     Bureaucrat b("Danny",20);
     Form f("NBA Contract", 12,13);
     Form g(f);
-    
+
     a.signForm(f);
     a.signForm(f);
     b.signForm(g);
     }
     try{
-        std::cout << "Test1:\nGenerate Form with grade out of range\n";
+        std::cout << "Test2:\nGenerate Form with grade out of range\n";
         Form a("Anthony",0, 10);
         std::cout << a;
     } catch (std::exception& e){
