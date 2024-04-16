05006ce6da48029c7bad29401bb4ee37eac4a351
fabileo <76965192+leonfabi@users.noreply.github.com>
2024-04-16 19:53:05 +0200

    cpp08/ex00 ex00 done

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,24 +1,16 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-#include <iterator>
 #include <algorithm>
 #include <exception>
 
-class NotFoundException : public std::exception {
-public:
-    virtual const char* what() const throw() {
-        return "Element not found in the container.";
-    }
-};
-
 template<typename T>
-typename T::iterator easyfind(T& container, int value) {
+int easyfind(T& container, int value) {
     typename T::iterator it = std::find(container.begin(), container.end(), value);
     if (it == container.end()) {
-        throw NotFoundException();
+        throw std::exception();
     }
-    return it;
+    return std::distance(container.begin(), it);
 }
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <list>
+#include <deque>
 #include "easyfind.hpp"
 
 int main() {
-    std::vector<int> numbers;
-    numbers.push_back(10);
-    numbers.push_back(20);
-    numbers.push_back(30);
-    numbers.push_back(40);
-    numbers.push_back(50);
+    int numbers[] = {340, 30, 20, 40, 50};
+    std::vector<int> numbers_vector(numbers, numbers + 5);
+    std::list<int> numbers_list(numbers, numbers + 5);
+    std::deque<int> numbers_deque(numbers, numbers + 5);
+
 
     try {
-        std::vector<int>::iterator found = easyfind(numbers, 30);
-        std::cout << "Found the number: " << *found << std::endl;
-        found = easyfind(numbers, 100); // This should throw an exception
-        std::cout << "Found the number: " << *found << std::endl;
-    } catch (const NotFoundException& e) {
-        std::cout << e.what() << std::endl;
+        std::cout << "Vector:" << std::endl;
+        int found = easyfind(numbers_vector, 30);
+        std::cout << "Found the number 30 at " << found << std::endl;
+        std::cout << "List:" << std::endl;
+        found = easyfind(numbers_list, 30);
+        std::cout << "Found the number 30 at " << found << std::endl;
+        std::cout << "Deque:" << std::endl;
+        found = easyfind(numbers_deque, 30);
+        std::cout << "Found the number 30 at " << found << std::endl;
+        found = easyfind(numbers_vector, 100); 
+        std::cout << "Found the number 100 at " << found << std::endl;
+    } catch (const std::exception& e) {
+        std::cout << "Number not found in the container" << std::endl;
     }
 
     return 0;
